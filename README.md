4ee7d66604b72c03809e69886878ae7b5a6e54ac
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-02-24 13:30:30 -0300

    Added index base and Updated README.md

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+# YoTube-BR
+Acesso:
+```
+https://yotube-br.herokuapp.com/
+```
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SearchBar from './Components/SearchBar';
+import VideoList from './Components/VideoList';
+import VideoDetail from './Components/VideoDetail';
+import YTSeach from 'youtube-api-search';
 import './App.css';
+import { Icon, notification } from 'antd';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      video: [],
+      search: true,
+      selectedVideo: {}
+    };
+    this.welcome();
+  }
+
+  welcome = () => {
+    notification.open({
+      message: 'Hey, nice to see you here',
+      description: 'Let us start by searching for some videos',
+      icon: <Icon type="smile" style={{color: '#108ee9'}} />
+    })
+  }
+
+  videoSearch(term){
+    if(this.state.search){
+      YTSeach({ key: API_KEY, term }, (data) =>{
+        try{
+          if(data && data.data && data.data.error.message){
+            console.log(data);
+            throw('error')
+          }
+          this.setState({ videos: data, selectedVideo: data[0] })
+          console.log(this.state.video);
+        }catch(error){
+          notification['error']({
+            message: 'Daily Limit Exceeded',
+            description: 'Youtube data API daily limit have exceeded. Quota will be recharged at 1:30pm IST. Wait till then.',
+          })
+        }
+      });
+    }
+  }
+
+  handleChange = (value) => {
+    setTimeout( () => {
+      if(value === ''){
+        this.setState({ videos: [], selectedVideo: null });
+        return;
+      }
+
+      if(this.state.search){
+        this.videoSearch(value);
+      }
+
+      setTimeout( () => {
+        this.setState({ search: true });
+      }, 5000);
+    }, 2000);
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ "display": "flex", "flexDirection": "column" }}>
+        <div style={{ "display": "flex", "justifyContent": "space-between", "background": "#123456" }}>
+          <h1 style={{ "color":"#fff", "alignSelf": "center", "flexBasis": "4", "paddingTop": "20px", "paddingLeft": "30px" }}>YTSeach <Icon type={"search"} /></h1>
+          <SearchBar videos={ this.state.videos } video={ this.state.selectedVideo } onChange={ this.handleChange } handleSearch={ (video) => { this.setState({ selectedVideo: this.state.videos[video], search: false }) } } />
+        </div>
+        <div style={{ "display": "flex" }}>
+          <VideoDetail video={ this.state.selectedVideo } />
+          <VideoList
+            videos={ this.state.videos }
+            onVideoSelect={ (userSelected) => { this.setState({ selectedVideo: this.state.videos[userSelected] }) }} 
+            />
+        </div>
       </div>
     );
   }
